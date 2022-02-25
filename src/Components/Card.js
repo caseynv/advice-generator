2bb8c14d25fc111649bd79290470e3ebbf84b170
Kelelchi Nwandu <nwandukelechi@gmail.com>
2022-02-25 06:16:46 +0100

    update

---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Card() {
+    return (
+        <>
+            <div className='body'>
+                <div className='card'>
+                    Hello
+                </div>
+            </div>
+            
+        </>
+    );
+  }
+
+
+export default Card;
