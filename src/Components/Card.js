894f17e11567e254a1e902370da0df0addd468bf
Kelelchi Nwandu <nwandukelechi@gmail.com>
2022-02-27 23:31:44 +0100

    update

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
+import axios from 'axios';
 
 function Card() {
+    const handleData = async (event) => {
+        event.preventDefault();
+        const resp = await axios.get("https://api.adviceslip.com/advice")
+        console.log(resp.json())
+        /*props.onSubmit(resp.data);
+    }; catch (error){
+        props.onSubmit(error)
+        setState({ userName: '' })*/
+    };
     return (
         <>
             <div className='body'>
                 <div className='card'>
                     Hello
+                </div>
+                <div onClick={handleData} className='button'>
+                    <Icon icon="ci:play-circle-outline" color="darkgreen" />
                 </div>
             </div>
             
