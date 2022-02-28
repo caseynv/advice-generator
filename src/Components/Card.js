5a71c2545c0e043087b53c239485304a56297a5a
Kelelchi Nwandu <nwandukelechi@gmail.com>
2022-02-28 20:45:07 +0100

    update

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { Icon } from '@iconify/react';
 import axios from 'axios';
 
 function Card() {
-    const handleData = async (event) => {
-        event.preventDefault();
-        const resp = await axios.get("https://api.adviceslip.com/advice")
-        console.log(resp.json())
-        /*props.onSubmit(resp.data);
-    }; catch (error){
-        props.onSubmit(error)
-        setState({ userName: '' })*/
-    };
-    return (
+/*
+    const [advicee, setAdvicee] = useState();
+        axios.get('https://api.adviceslip.com/advice')
+        .then((resp) => {
+           
+            console.log(resp.data.slip.id)
+            console.log(resp.data.slip.advice)
+        })*/
+
+        return (
         <>
             <div className='body'>
                 <div className='card'>
-                    Hello
+                    <div className='advice'>
+                        <p>ADVICE </p>
+                        <p>Advice</p>
+                    </div>
+                    <div className='row cute-design'>
+                        <div className='col-5'>
+                            <hr></hr>
+                        </div>
+                        <div className='col-1 no-gut'>
+                            <Icon icon="fluent:pause-16-filled" color="white" width="30" height="30" />
+                        </div>
+                        <div className='col-5'>
+                            <hr></hr>
+                        </div>
+                    </div>
+                    <button className='button'>
+                        <Icon icon="fluent:record-stop-32-filled" color="#5af65a" width="30" height="30" />
+                    </button>
+                    
                 </div>
-                <div onClick={handleData} className='button'>
-                    <Icon icon="ci:play-circle-outline" color="darkgreen" />
-                </div>
+                
             </div>
             
         </>
